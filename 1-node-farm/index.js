33aae90bdbb8ce0ff65a3f12c0ac9d2882a5fa0a
Leandro Reis <leandro.reiz@gmail.com>
2022-05-01 14:38:12 +0100

    Using modules (core and third party), creating your own modules

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,7 +1,11 @@
-// Importing modules
+// Core modules
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+// Third party modules
+const slugify = require('slugify');
+// My own modules
+const replaceTemplate = require('./modules/replaceTemplate');
 
 /*
 // -----------------------------------------------
@@ -45,28 +49,8 @@ fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
 console.log('Will read the file...');
 */
 // -----------------------------------------------
-// Server
+// NODE FARM APPLICATION
 // -----------------------------------------------
-// Code on top level is executed only on first call
-// Reading the file synchronously
-
-// Function to replace teamplates placeholders
-// -----------------------------------------------
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%NAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-
-  return output;
-};
 
 // Loading templates
 // -----------------------------------------------
@@ -85,34 +69,42 @@ const tempProduct = fs.readFileSync(
   'utf-8'
 );
 
-// Load data
+// Reading the file synchronously
+// Code on top level is executed only on first call
 // -----------------------------------------------
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
 
-// Code inside the server is executed for every request
+// -----------------------------------------------
+// SERVER
+// -----------------------------------------------
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  // Code inside the server is executed for every request
+  const { query, pathname } = url.parse(req.url, true);
 
   // Routes
   // ---------------------------------------------
   // Overview page
-  if (pathName === '/' || pathName === '/overview') {
+  if (pathname === '/' || pathname === '/overview') {
     res.writeHead(200, { 'Content-type': 'text/html' });
 
     const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el));
-    console.log(cardsHtml);
-
     const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
 
     res.end(output);
 
     // Product page
-  } else if (pathName === '/product') {
-    res.end('This is the PRODUCT!');
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+
+    res.end(output);
 
     // Application Programming Interface (API)
-  } else if (pathName === '/api') {
+  } else if (pathname === '/api') {
     res.writeHead(200, { 'Content-type': 'application/json' });
     res.end(data);
 
@@ -126,6 +118,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
+// Server will be listening
 server.listen(8000, '127.0.0.1', () => {
   console.log('Listening to request on port 8000...');
 });
---
+++ 1-node-farm/modules/replaceTemplate.js
@@ -0,0 +1,20 @@
+// Function to replace teamplates placeholders
+// -----------------------------------------------
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%NAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+
+  return output;
+};
+
+// Exports the function above
+module.exports = replaceTemplate;
