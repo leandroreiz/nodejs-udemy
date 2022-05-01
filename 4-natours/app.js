6b2d5e8a1d059f91e469adffbcc5cf2629870106
Leandro Reis <leandro.reiz@gmail.com>
2022-05-01 21:58:10 +0100

    [Natours] Add CRUD routes

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,24 +1,124 @@
-/**
- * A REST API (also known as RESTful API) is an application programming interface (API or web API) that conforms to the constraints of REST architectural style and allows for interaction with RESTful web services. REST stands for representational state transfer.
- */
-
+// ----------------------------------------------
+// NATOURS APP
+// Ver.: 1.0.0
+// Design: Jonas Schmedtmann
+// Coded by Leandro Reis
+// ----------------------------------------------
+// Imports
+// ----------------------------------------------
 const express = require('express');
+const fs = require('fs');
 
+// ----------------------------------------------
+// Global variables
+// ----------------------------------------------
 const app = express();
 const port = 3000;
 
-// Methods
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
-});
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
-app.post('/', (req, res) => {
-  res.status(200).send('You can post to this endpoint :)');
-});
+// ----------------------------------------------
+// Middleware
+// ----------------------------------------------
+app.use(express.json());
 
+// ----------------------------------------------
+// Get all tours
+// ----------------------------------------------
+const getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+// ----------------------------------------------
+// Get a tour
+// ----------------------------------------------
+const getTour = (req, res) => {
+  const tour = tours.find((tour) => tour.id === +req.params.id);
+
+  try {
+    if (!tour) throw new Error('Invalid ID');
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error.message,
+    });
+  }
+};
+
+// ----------------------------------------------
+// Create new tour
+// ----------------------------------------------
+const createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+
+// ----------------------------------------------
+// Update tour data
+// ----------------------------------------------
+const updateTour = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour should go here...>',
+    },
+  });
+};
+
+// ----------------------------------------------
+// Delete tour
+// ----------------------------------------------
+const deleteTour = (req, res) => {
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+// ----------------------------------------------
+// Routes
+// ----------------------------------------------
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+// ----------------------------------------------
 // Start server
+// ----------------------------------------------
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
