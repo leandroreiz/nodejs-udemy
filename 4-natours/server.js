45d112bc0fc6b05652eff8262fa71617ca30364c
Leandro Reis <leandro.reiz@gmail.com>
2022-05-03 21:35:05 +0100

    Connect to MongoDB, create tour schema

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,11 +1,67 @@
 // ----------------------------------------------
 // Server
 // ----------------------------------------------
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const db = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+// ----------------------------------------------
+// Database connection
+// ----------------------------------------------
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Database connected!'));
+
+// ----------------------------------------------
+// Create schema
+// ----------------------------------------------
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+// ----------------------------------------------
+// Create model
+// ----------------------------------------------
+const Tour = mongoose.model('Tour', tourSchema);
+
+// Creating an instance of the Tour model
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log(`Error:`, err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
