8a133aa6fe7382925b8d7ef19db702f77749ebe7
Leandro Reis <leandro.reiz@gmail.com>
2022-05-05 20:32:31 +0100

    Add pagination using queries

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -38,6 +38,20 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 4. Pagination
+    const page = +req.query.page || 1;
+    const limit = +req.query.limit || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) {
+        throw new Error('This page does not exist');
+      }
+    }
+
     // ------------------------------------------
     // Execute query
     const tours = await query;
