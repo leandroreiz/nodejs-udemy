a645b68a70cee0eb489099205b036632d1cb209a
Leandro Reis <leandro.reiz@gmail.com>
2022-05-05 21:33:42 +0100

    Refactor API Features

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -2,6 +2,7 @@
 // Imports
 // ----------------------------------------------
 const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
 
 // ----------------------------------------------
 // Get all tours
@@ -10,51 +11,13 @@ exports.getAllTours = async (req, res) => {
   try {
     // ------------------------------------------
     // Build query
-    // 1a. Filtering
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
 
-    excludedFields.forEach(el => delete queryObj[el]);
-
-    // 1b. Advanced Filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/(gte|gt|lte|lt)\b/g, match => `$${match}`);
-
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // 2. Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    // 3. Field limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    // 4. Pagination
-    const page = +req.query.page || 1;
-    const limit = +req.query.limit || 100;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    if (page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) {
-        throw new Error('This page does not exist');
-      }
-    }
-
-    // ------------------------------------------
-    // Execute query
-    const tours = await query;
+    const tours = await features.query;
 
     // ------------------------------------------
     // Send response
