0fc0294e891b675cac59e8a44df7b11bf225a9b3
Leandro Reis <leandro.reiz@gmail.com>
2022-05-09 23:33:48 +0100

    Add a class to deal with error messages and status and a controller to deal with the error

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -9,6 +9,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -34,24 +36,9 @@ app.use('/api/v1/users', userRouter);
 
 // Dealing with unknown urls
 app.all('*', (req, res, next) => {
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.status = 'fail';
-  err.statusCode = 404;
-
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-// ----------------------------------------------
-// Error Handling Middleware
-// ----------------------------------------------
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ 4-natours/controllers/errorController.js
@@ -0,0 +1,12 @@
+// ----------------------------------------------
+// Error Handling Middleware
+// ----------------------------------------------
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
