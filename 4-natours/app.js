f29b29ceed464964cac97098d0dccf00aef0f69e
Leandro Reis <leandro.reiz@gmail.com>
2022-05-09 22:54:33 +0100

    Add error handling middleware

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -34,12 +34,24 @@ app.use('/api/v1/users', userRouter);
 
 // Dealing with unknown urls
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`,
-  });
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
 
-  next();
+  next(err);
+});
+
+// ----------------------------------------------
+// Error Handling Middleware
+// ----------------------------------------------
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
 });
 
 module.exports = app;
