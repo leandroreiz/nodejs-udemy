0b8b0e959176d55d5e5496d6ad0e96c0f43074a3
Leandro Reis <leandro.reiz@gmail.com>
2022-05-11 23:30:06 +0100

    Fix bug on handleCastErrorDB where the path and value were undefined

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -1,6 +1,19 @@
 // ----------------------------------------------
+// Imports
+// ----------------------------------------------
+
+const clone = require('clone');
+const AppError = require('../utils/appError');
+
+// ----------------------------------------------
 // Error Handling Middleware
 // ----------------------------------------------
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -13,17 +26,15 @@ const sendErrorDev = (err, res) => {
 const sendErrorProd = (err, res) => {
   // Operational, trusted error
   // Send message to client
-  if (err.isOperational)
+  if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
     });
-
+  }
   // Programming or some unknown error
   // Don't leak error details to client
-  if (!err.isOperational) {
-    console.error('⚠️ERROR', err);
-
+  else {
     res.status(500).json({
       status: 'error',
       message: 'Something went wrong!',
@@ -37,5 +48,10 @@ module.exports = (err, req, res, next) => {
 
   if (process.env.NODE_ENV === 'development') sendErrorDev(err, res);
 
-  if (process.env.NODE_ENV === 'production') sendErrorProd(err, res);
+  if (process.env.NODE_ENV === 'production') {
+    let error;
+    if (err.name === 'CastError') error = handleCastErrorDB(err);
+    sendErrorProd(error, res);
+    // @BUG / FIXED: but still need to check if it is okay to use 'err' to pass as a parameter to 'handleCastErrorDB'
+  }
 };
