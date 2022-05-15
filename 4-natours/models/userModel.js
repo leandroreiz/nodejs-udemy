9faa5d16f0a1b56b83f0d71548083923f0fe88f3
Leandro Reis <leandro.reiz@gmail.com>
2022-05-15 11:43:25 +0100

    Add password encryption with bcryptjs

--- 4-natours/models/userModel.js
+++ 4-natours/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 // ----------------------------------------------
 // Create schema
@@ -28,7 +29,24 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      // this validation works only on create() or save()
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'The passwords must match',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // Run this funcion only if password was modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password and delete passwordConfirm
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = null; // undefined
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
