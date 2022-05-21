b7f19ed91bf52a343b006300231a6a21a46da347
Leandro Reis <leandro.reiz@gmail.com>
2022-05-21 17:27:05 +0100

    Add child referencing to tours document (guides)

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const User = require('./userModel');
 
 // ----------------------------------------------
 // Create schema
@@ -105,7 +104,12 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
-    guides: Array,
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
@@ -127,13 +131,6 @@ tourSchema.virtual('durationInWeeks').get(function () {
 // ----------------------------------------------
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
-  next();
-});
-
-tourSchema.pre('save', async function (next) {
-  const guidesPromises = this.guides.map(async id => await User.findById(id));
-  this.guides = await Promise.all(guidesPromises);
-
   next();
 });
 
