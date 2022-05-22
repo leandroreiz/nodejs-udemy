296b16fdd8c9754fac4623f5bdf0b4ca381c7e70
Leandro Reis <leandro.reiz@gmail.com>
2022-05-22 18:50:26 +0100

    Add review model

---
+++ 4-natours/models/reviewModel.js
@@ -0,0 +1,52 @@
+// ----------------------------------------------
+// Imports
+// ----------------------------------------------
+
+const mongoose = require('mongoose');
+
+// ----------------------------------------------
+// Create schema
+// ----------------------------------------------
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review can not be empty!'],
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'The tour can not be empty!'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'The user can not be empty!'],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+// ----------------------------------------------
+// Create model
+// ----------------------------------------------
+
+const Review = mongoose.model('Review', reviewSchema);
+
+// ----------------------------------------------
+// Exports
+// ----------------------------------------------
+
+module.exports = Review;
