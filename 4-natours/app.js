043b7f88ebb340ed58753df4cb38d422e4ebadc0
Leandro Reis <leandro.reiz@gmail.com>
2022-05-31 21:46:46 +0100

    Add a map with Leaflet

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -38,8 +38,30 @@ app.set('views', path.join(__dirname, 'views'));
 // Global Middlewares
 // ----------------------------------------------
 
-// Set security HTTP headers
-app.use(helmet());
+// Further HELMET configuration for Security Policy (CSP)
+const scriptSrcUrls = ['https://unpkg.com/', 'https://tile.openstreetmap.org'];
+const styleSrcUrls = [
+  'https://unpkg.com/',
+  'https://tile.openstreetmap.org',
+  'https://fonts.googleapis.com/',
+];
+const connectSrcUrls = ['https://unpkg.com', 'https://tile.openstreetmap.org'];
+const fontSrcUrls = ['fonts.googleapis.com', 'fonts.gstatic.com'];
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [],
+      connectSrc: ["'self'", ...connectSrcUrls],
+      scriptSrc: ["'self'", ...scriptSrcUrls],
+      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", 'blob:'],
+      objectSrc: [],
+      imgSrc: ["'self'", 'blob:', 'data:', 'https:'],
+      fontSrc: ["'self'", ...fontSrcUrls],
+    },
+  })
+);
 
 // Serving static files (works closely with PUG)
 app.use(express.static(path.join(__dirname, 'public')));
