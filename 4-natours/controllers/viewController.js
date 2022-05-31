7f36c1fe61986d5f9ad1250c88f89dcebc178ff3
Leandro Reis <leandro.reiz@gmail.com>
2022-05-31 11:42:54 +0100

    Build tour overview - card layout

--- 4-natours/controllers/viewController.js
+++ 4-natours/controllers/viewController.js
@@ -1,8 +1,28 @@
-exports.getOverview = (req, res) => {
+// ----------------------------------------------
+// Imports
+// ----------------------------------------------
+
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+// ----------------------------------------------
+// Overview
+// ----------------------------------------------
+
+exports.getOverview = catchAsync(async (req, res) => {
+  // Get tour data from collection
+  const tours = await Tour.find();
+
+  // Render template using tour data
   res.status(200).render('overview', {
     title: 'All tours',
+    tours,
   });
-};
+});
+
+// ----------------------------------------------
+// Get tour
+// ----------------------------------------------
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
